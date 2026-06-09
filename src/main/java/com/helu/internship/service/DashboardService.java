462ae2496ec036e54bbcc2a1c6cf691eead74aa0
Huynh Le Duc Anh <huynhdem2609@gmail.com>
2026-06-09 12:19:56 +0700

    nuthing

--- src/main/java/com/helu/internship/service/DashboardService.java
+++ src/main/java/com/helu/internship/service/DashboardService.java
@@ -5,6 +5,7 @@ import com.helu.internship.dto.response.LeadStatusCountResponse;
 
 import java.util.List;
 
+
 public interface DashboardService {
     List<LeadStatusCountResponse> getLeadStatusCount();
     List<LeadByStatusResponse> getLeadsByStatus(String status);
