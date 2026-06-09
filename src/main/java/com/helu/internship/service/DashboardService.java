3452c9901b44a11319db83c499172f81d243adaf
BaoFuJi <05nguyenngocgiabao77.2@gmail.com>
2026-06-09 11:33:59 +0700

    gộp code Hà-Bảo

--- src/main/java/com/helu/internship/service/DashboardService.java
+++ src/main/java/com/helu/internship/service/DashboardService.java
@@ -1,6 +1,9 @@
 package com.helu.internship.service;
 
+
+import com.helu.internship.dto.response.LeadByStatusResponse;
 import com.helu.internship.dto.response.LeadSourceCostProjection;
+import com.helu.internship.dto.response.LeadStatusCountResponse;
 import com.helu.internship.dto.response.PipelineCoverageProjection;
 
 import java.util.List;
@@ -10,4 +13,7 @@ public interface DashboardService {
     Double getAverageDaysToWon();
     List<LeadSourceCostProjection> getLeadSourceCostDashboard();
     List<PipelineCoverageProjection> getPipelineCoverage();
+    List<LeadStatusCountResponse> getLeadStatusCount();
+    List<LeadByStatusResponse> getLeadsByStatus(String status);
+
 }
--- src/main/java/com/helu/internship/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/helu/internship/service/impl/DashboardServiceImpl.java
@@ -1,36 +1,61 @@
 package com.helu.internship.service.impl;
 
+import com.helu.internship.dto.response.LeadByStatusResponse;
 import com.helu.internship.dto.response.LeadSourceCostProjection;
+import com.helu.internship.dto.response.LeadStatusCountResponse;
 import com.helu.internship.dto.response.PipelineCoverageProjection;
 import com.helu.internship.repo.CostPerLeadRepo;
+import com.helu.internship.repo.LeadRepo;
 import com.helu.internship.repo.LeadStatusHistoryRepo;
+import com.helu.internship.repo.PipelineCoveragerRepo;
 import com.helu.internship.service.DashboardService;
 import org.springframework.stereotype.Service;
-import com.helu.internship.repo.PipelineCoveragerRepo;
 
 import java.util.List;
 
 @Service
-public  class DashboardServiceImpl implements DashboardService {
+public class DashboardServiceImpl implements DashboardService {
+
+    private final LeadRepo leadRepo;
     private final LeadStatusHistoryRepo leadStatusHistoryRepo;
     private final CostPerLeadRepo costPerLeadRepo;
     private final PipelineCoveragerRepo pipelineCoverageRepo;
 
-    public DashboardServiceImpl(LeadStatusHistoryRepo leadStatusHistoryRepo,
-                                CostPerLeadRepo costPerLeadRepo,
-                                PipelineCoveragerRepo pipelineCoverageRepo) {
+    public DashboardServiceImpl(
+            LeadRepo leadRepo,
+            LeadStatusHistoryRepo leadStatusHistoryRepo,
+            CostPerLeadRepo costPerLeadRepo,
+            PipelineCoveragerRepo pipelineCoverageRepo) {
+
+        this.leadRepo = leadRepo;
         this.leadStatusHistoryRepo = leadStatusHistoryRepo;
         this.costPerLeadRepo = costPerLeadRepo;
         this.pipelineCoverageRepo = pipelineCoverageRepo;
     }
 
     @Override
+    public List<LeadStatusCountResponse> getLeadStatusCount() {
+        return leadRepo.countLeadByStatus()
+                .stream()
+                .map(row -> new LeadStatusCountResponse(
+                        String.valueOf(row[0]),
+                        (Long) row[1]
+                ))
+                .toList();
+    }
+
+    @Override
+    public List<LeadByStatusResponse> getLeadsByStatus(String status) {
+        return leadRepo.findLeadsByStatus(status);
+    }
+
+    @Override
     public List<LeadSourceCostProjection> getLeadSourceCostDashboard() {
         return costPerLeadRepo.getCostPerLeadBySource();
     }
+
     @Override
     public Double getAverageDaysToWon() {
-
         return leadStatusHistoryRepo.getAverageDaysToWon();
     }
 
@@ -38,5 +63,4 @@ public  class DashboardServiceImpl implements DashboardService {
     public List<PipelineCoverageProjection> getPipelineCoverage() {
         return pipelineCoverageRepo.getPipelineCoverage();
     }
-
 }
