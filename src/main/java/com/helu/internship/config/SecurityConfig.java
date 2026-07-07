11d730c85b32ca8ebc49f5852da2d2e916907012
Khanh Truong <sbt0918203277@gmail.com>
2026-07-07 22:40:13 +0700

    update mới nhất

--- src/main/java/com/helu/internship/config/SecurityConfig.java
+++ src/main/java/com/helu/internship/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
             )
             .logout(logout -> logout
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
+                .logoutSuccessUrl("/?lang=en")
                 .permitAll()
             );
         return http.build();
